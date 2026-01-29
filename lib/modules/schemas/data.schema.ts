9ecb8f2e59ab6a752eb1061db0ab2ba311c6f010
capSaymon <szymonpietruszka@onet.pl>
2026-01-29 12:56:15 +0100

    dodanie likes do schematu

--- lib/modules/schemas/data.schema.ts
+++ lib/modules/schemas/data.schema.ts
@@ -5,6 +5,7 @@ export const DataSchema: Schema = new Schema({
    title: { type: String, required: true },
    text: { type: String, required: true },
    image: { type: String, required: true },
+   likes: { type: Number, default: 0 },
 });
 
 export default model<IData>('Post-XYZ', DataSchema);
